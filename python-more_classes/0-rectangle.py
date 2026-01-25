445ac1dee85abe54cf7a9c69988f823691d05979
manaralthqfiu <9vieo677@gmail.com>
2026-01-25 10:47:14 +0000

    add the code for the 0 task

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines an empty Rectangle class."""
+
 class Rectangle:
-    """Empty class that defines a rectangle"""
+    """Empty class that defines a rectangle."""
     pass
