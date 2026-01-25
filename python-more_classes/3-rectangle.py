940d7870620d7f35c092586e54759c9c37348805
manaralthqfiu <9vieo677@gmail.com>
2026-01-25 15:17:06 +0000

    add code for the 3th task

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -46,8 +46,10 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
+
     def __str__(self):
-    if self.width == 0 or self.height == 0:
-        return ""
-    row = "#" * self.width
-    return "\n".join(row for _ in range(self.height))
+        """Return the rectangle as a string of # characters."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        row = "#" * self.width
+        return "\n".join(row for _ in range(self.height))
