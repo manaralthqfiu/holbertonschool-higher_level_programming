d10bb059bb63b62a550d5be8bf65e6c8c87d67e9
manaralthqfiu <9vieo677@gmail.com>
2026-01-29 07:37:03 +0000

    add the code for the 0th task

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function that returns the list of available attributes and methods of an object."""
+"""Function that returns attributes and methods of an object."""
 
 
 def lookup(obj):
