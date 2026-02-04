308b670c4452240387772adc9804230f7d69f347
manaralthqfiu <9vieo677@gmail.com>
2026-02-04 12:19:34 +0000

    fix the 0th task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module that contains the read_file function.
+Reads a UTF-8 text file and prints its content to stdout.
+"""
+
 def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
